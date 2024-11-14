350f31f0d55d24bdd6d178ad4df6c9b09e4409c2
CharlesOkechukwu <realcharlieok@gmail>
2024-11-14 18:23:27 +0100

    implement add epc, edit epc, delete epc, get epc, view epc, eidt and delete items

---
+++ api/models/escrow_items.py
@@ -0,0 +1,15 @@
+"""Escrow Purchase Contract Items Model Module"""
+from api.app import db
+
+
+class EPCItem(db.Model):
+    """Escrow Purchase Contract Items model"""
+    __tablename__ = 'epc_items'
+    id = db.Column(db.Integer, primary_key=True)
+    escrow_uid = db.Column(db.String(300), db.ForeignKey('escrow_purchase_contracts.escrow_uid'), nullable=False)
+    product_name = db.Column(db.String(300), nullable=True)
+    product_code = db.Column(db.String(300), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    total = db.Column(db.Float, nullable=False)
+    description = db.Column(db.String(300), nullable=True)
---
+++ api/models/escrow_purchase_contract.py
@@ -0,0 +1,20 @@
+"""Module for Escrow Purchase Contract Model"""
+from api.app import db
+
+
+class EscrowPurchaseContract(db.Model):
+    """Escrow purchase contract model class."""
+    __tablename__ = 'escrow_purchase_contracts'
+    id = db.Column(db.Integer, primary_key=True)
+    escrow_uid = db.Column(db.String(300), unique=True, nullable=False)
+    seller_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    seller_name = db.Column(db.String(300), nullable=False)
+    buyer_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=True)
+    buyer_name = db.Column(db.String(300), nullable=True)
+    created_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp())
+    updated_at = db.Column(db.DateTime, nullable=False, default=db.func.current_timestamp(), onupdate=db.func.current_timestamp())
+    delivery_fee = db.Column(db.Float, nullable=False)
+    delivery_date = db.Column(db.DateTime, nullable=False)
+    total_amount = db.Column(db.Float, nullable=False)
+    status = db.Column(db.String(300), nullable=False)
+    items = db.relationship('EPCItem', backref='escrow_purchase_contract', lazy=True)
